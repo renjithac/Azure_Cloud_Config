99d6f13e415912df7b60df07f1ec1f4a7739e1e6
Renjith AC <acrenjith@gmail.com>
2021-02-26 14:52:59 +0530

    Newcommit4

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 provider "azurerm"{
     features {}
     
- subscription_id = "${var.subscription_id}"  
- client_id       = "${var.client_id}"
- client_secret   = "${var.client_secret}"
- tenant_id       = "${var.tenant_id}"
+ subscription_id = ""  
+ client_id       = ""
+ client_secret   = ""
+ tenant_id       = ""
 }
- variable "subscription_id"{
-          description="Enter subscription id"
-    }
- variable "client_id"{
-       description="Enter client id" 
-    }
-     variable "client_secret"{
-        description="Enter client secret"
-    }
-     variable "tenant_id"{
-        description="Enter tenant id"
-    }
+#  variable "subscription_id"{
+#           description="Enter subscription id"
+#     }
+#  variable "client_id"{
+#        description="Enter client id" 
+#     }
+#      variable "client_secret"{
+#         description="Enter client secret"
+#     }
+#      variable "tenant_id"{
+#         description="Enter tenant id"
+#     }
