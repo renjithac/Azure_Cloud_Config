04044732e079e055b27c0f5ff432c03c4656344c
Renjith AC <acrenjith@gmail.com>
2021-02-26 15:27:51 +0530

    nc5

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm"{
     features {}
     
- subscription_id = var.subscriptionID
- client_id       = var.client_id
- client_secret   =var.client_secret
- tenant_id       = var.tenant_id
+#  subscription_id = var.subscriptionID
+#  client_id       = var.client_id
+#  client_secret   =var.client_secret
+#  tenant_id       = var.tenant_id
 
 }
 
