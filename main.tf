07a5593bad3930a03b96ef7478c59df16d65f3ee
Renjith AC <acrenjith@gmail.com>
2021-02-26 16:34:40 +0530

    FC7

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
    // version = "1.38.0"
-    subscription_id = var.subscription_id
+    //subscription_id = var.subscription_id
 }
 
 resource "azurerm_resource_group" "DevRG" {
