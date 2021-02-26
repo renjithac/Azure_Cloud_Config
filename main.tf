5642ddfad5ec68ae2abecc17860cde5cd192b45c
Renjith AC <acrenjith@gmail.com>
2021-02-26 14:41:35 +0530

    new commit

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm"{
     features {}
     
-#  subscription_id = "${var.subscription_id}"  
-#  client_id       = "${var.client_id}"
-#  client_secret   = "${var.client_secret}"
-#  tenant_id       = "${var.tenant_id}"
+  subscription_id = "${var.subscription_id}"  
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 resource "azurerm_resource_group" "resource_gp" {
   name     = "DemoApp2"
