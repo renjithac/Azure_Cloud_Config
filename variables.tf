426a559ec6fd8bd895ee9d398bfaa22b194d0985
Renjith AC <acrenjith@gmail.com>
2021-02-26 15:29:11 +0530

    nc6

--- variables.tf
+++ variables.tf
@@ -7,20 +7,20 @@
 #  tenant_id       = ""
 # }
 
- variable "client_id"{
-      type = string
-       description="Enter client id" 
-    }
-     variable "client_secret"{
-         type = string
-        description="Enter client secret"
-    }
-     variable "tenant_id"{
-         type = string
-        description="Enter tenant id"
-    }
+#  variable "client_id"{
+#       type = string
+#        description="Enter client id" 
+#     }
+#      variable "client_secret"{
+#          type = string
+#         description="Enter client secret"
+#     }
+#      variable "tenant_id"{
+#          type = string
+#         description="Enter tenant id"
+#     }
   
-variable "subscriptionID" {
-    type = string
-    description = "Variable for our resource group"
-}
+# variable "subscriptionID" {
+#     type = string
+#     description = "Variable for our resource group"
+# }
