cb30bf5c6739d343d6a7112ec69970b904370849
Renjith AC <acrenjith@gmail.com>
2021-02-26 14:39:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,14 @@ terraform {
   }
 }
 
-
+provider "azurerm"{
+    features {}
+    
+#  subscription_id = "${var.subscription_id}"  
+#  client_id       = "${var.client_id}"
+#  client_secret   = "${var.client_secret}"
+#  tenant_id       = "${var.tenant_id}"
+}
 resource "azurerm_resource_group" "resource_gp" {
   name     = "DemoApp2"
   location = "eastus"
